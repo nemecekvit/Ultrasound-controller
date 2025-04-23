8ba3cf2b25f9a57948c92ba2ef7e0341e1b8cacb
lokit58 <vitecek73@gmail.com>
2025-04-23 15:31:04 +0200

    Reverted to old sensor select

--- src/sensor_select.vhd
+++ src/sensor_select.vhd
@@ -1,7 +1,5 @@
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity sensor_select is
     Port ( sens_in_1  : in STD_LOGIC_VECTOR(11 downto 0);
@@ -9,71 +7,37 @@ entity sensor_select is
            sens_in_3  : in STD_LOGIC_VECTOR(11 downto 0);
            sens_in_4  : in STD_LOGIC_VECTOR(11 downto 0);
            select_in  : in STD_LOGIC_VECTOR(3 downto 0);
-           avg_in     : in STD_LOGIC;
            data_out   : out STD_LOGIC_VECTOR(11 downto 0);
            select_out : out STD_LOGIC_VECTOR(3 downto 0)
     );
 end sensor_select;
 
 architecture Behavioral of sensor_select is
-
-    signal sig_avg : STD_LOGIC_VECTOR(47 downto 0);
-    signal avg_out : STD_LOGIC_VECTOR(13 downto 0);
-
 begin
 
-    process (select_in, sens_in_1, sens_in_2, sens_in_3, sens_in_4) is
+    process (select_in) is
     begin
-        if avg_in = '0' then
+        case select_in is
+            when "0001" =>
+                select_out <= select_in;
+                data_out <= sens_in_1;
+                  
+            when "0010" =>
+                select_out <= select_in;
+                data_out <= sens_in_2;
             
-            select_out <= select_in;
+            when "0100" =>
+                select_out <= select_in;
+                data_out <= sens_in_3;
+            
+            when "1000" =>
+                select_out <= select_in;
+                data_out <= sens_in_4;            
 
-            case select_in is
-                when "0001" =>
-                    data_out <= sens_in_1;
-                      
-                when "0010" =>
-                    data_out <= sens_in_2;
-                
-                when "0100" =>
-                    data_out <= sens_in_3;
-                
-                when "1000" =>
-                    data_out <= sens_in_4;            
-    
-                when others =>
-                    select_out <= (others => '0');
-                    data_out <= (others => '0');
-            end case;
-        else     
-
-            sig_avg <= b"000_000_000_000" & sig_avg(47 downto 12);
-            select_out <= select_in;
-
-            case select_in is
-                when "0001" =>        
-                    sig_avg(47 downto 36) <= sens_in_1;
-                      
-                when "0010" =>
-                    sig_avg(47 downto 36) <= sens_in_2;
-                
-                when "0100" =>
-                    sig_avg(47 downto 36) <= sens_in_3;
-                
-                when "1000" =>
-                    sig_avg(47 downto 36) <= sens_in_4;        
-    
-                when others =>
-                    select_out <= (others => '0');
-                    sig_avg <= (others => '0');
-                    data_out <= (others => '0');
-            end case;
-
-            avg_out <= sig_avg(47 downto 36) + sig_avg(35 downto 24) + sig_avg(23 downto 12) + sig_avg(11 downto 0);
-            avg_out <= b"00" & avg_out(13 downto 2);
-            data_out <= avg_out(11 downto 0);
-
-        end if;        
+            when others =>
+                select_out <= (others => '0');
+                data_out <= (others => '0');
+        end case;
     end process;
 
 end Behavioral;
--- test_benches/sensor_select_tb.vhd
+++ test_benches/sensor_select_tb.vhd
@@ -1,7 +1,7 @@
 -- Testbench automatically generated online
 -- at https://vhdl.lapinoo.net
--- Generation date : Mon, 21 Apr 2025 13:54:11 GMT
--- Request id : cfwk-fed377c2-68064e032b49a
+-- Generation date : Sat, 05 Apr 2025 14:12:39 GMT
+-- Request id : cfwk-fed377c2-67f13a576e7db
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -17,7 +17,6 @@ architecture tb of tb_sensor_select is
               sens_in_3  : in std_logic_vector (11 downto 0);
               sens_in_4  : in std_logic_vector (11 downto 0);
               select_in  : in std_logic_vector (3 downto 0);
-              avg_in     : in std_logic;
               data_out   : out std_logic_vector (11 downto 0);
               select_out : out std_logic_vector (3 downto 0));
     end component;
@@ -27,7 +26,6 @@ architecture tb of tb_sensor_select is
     signal sens_in_3  : std_logic_vector (11 downto 0);
     signal sens_in_4  : std_logic_vector (11 downto 0);
     signal select_in  : std_logic_vector (3 downto 0);
-    signal avg_in     : std_logic;
     signal data_out   : std_logic_vector (11 downto 0);
     signal select_out : std_logic_vector (3 downto 0);
 
@@ -39,46 +37,36 @@ begin
               sens_in_3  => sens_in_3,
               sens_in_4  => sens_in_4,
               select_in  => select_in,
-              avg_in     => avg_in,
               data_out   => data_out,
               select_out => select_out);
 
     stimuli : process
     begin
         -- ***EDIT*** Adapt initialization as needed
-        sens_in_1 <= (others => '0');
-        sens_in_2 <= (others => '0');
-        sens_in_3 <= (others => '0');
-        sens_in_4 <= (others => '0');
-        select_in <= (others => '0');
-        avg_in <= '0';
+        sens_in_1 <= x"001";
+        sens_in_2 <= x"002";
+        sens_in_3 <= x"003";
+        sens_in_4 <= x"004";
+        select_in <= "0000";
 
         -- ***EDIT*** Add stimuli here
 
         wait for 10 ns;
-        avg_in <= '1';
         select_in <= "0001";
 
         wait for 10 ns;
-        sens_in_1 <= x"001";
+        select_in <= "0010";
 
         wait for 10 ns;
-        sens_in_1 <= x"003";
+        select_in <= "0100";
 
         wait for 10 ns;
-        sens_in_1 <= x"001";
+        select_in <= "1000";
 
         wait for 10 ns;
-        sens_in_1 <= x"003";
+        select_in <= "0011";
 
         wait for 10 ns;
-        sens_in_1 <= x"002";
-
-        wait for 10 ns;
-
-
-
-
 
         wait;
     end process;
